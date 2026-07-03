bd49ef8ffdc4fb66b28722185008572b58b3860a
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2026-07-03 22:44:05 +0300

    fix of composite primary key

--- internal/database/models/event.go
+++ internal/database/models/event.go
@@ -8,13 +8,14 @@ import (
 )
 
 type Event struct {
-	ID        uuid.UUID  `gorm:"column:id;type:char(36);primaryKey" json:"event_id"`
-	ChainID   uuid.UUID  `gorm:"column:chain_id;type:char(36);primaryKey" json:"chain_id"`
-	ParentID  *uuid.UUID `gorm:"column:parent_id;type:char(36)" json:"parent_id,omitempty"`
-	Payload   string     `gorm:"column:payload;type:text;not null" json:"payload"`
-	CreatedAt time.Time  `gorm:"column:created_at;not null" json:"created_at"`
+	ID           uuid.UUID  `gorm:"column:id;type:char(36);primaryKey" json:"event_id"`
+	ChainName    string     `gorm:"column:chain_name;size:128;not null" json:"chain_name"`
+	ChainVaultID uuid.UUID  `gorm:"column:chain_vault_id;type:char(36);not null" json:"chain_vault_id"`
+	ParentID     *uuid.UUID `gorm:"column:parent_id;type:char(36)" json:"parent_id,omitempty"`
+	Payload      string     `gorm:"column:payload;type:text;not null" json:"payload"`
+	CreatedAt    time.Time  `gorm:"column:created_at;not null" json:"created_at"`
 
-	Chain  *Chain `gorm:"foreignKey:ChainID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"-"`
+	Chain  *Chain `gorm:"foreignKey:ChainName,ChainVaultID;references:Name,VaultID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"-"`
 	Parent *Event `gorm:"foreignKey:ParentID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL" json:"-"`
 }
 
